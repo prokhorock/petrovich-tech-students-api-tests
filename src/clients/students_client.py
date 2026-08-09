921b3d305e09628786987d4a192360e60d5cf620
denisprohorenko <prokhorenko.deniss@gmail.com>
2026-08-09 16:48:44 +0500

    add full students CRUD

--- src/clients/students_client.py
+++ src/clients/students_client.py
@@ -4,18 +4,34 @@ from src.config import BASE_URL
 
 
 class StudentsClient:
-    def __init__(self):
+    def __init__(self, timeout: int = 10):
         self.session = requests.Session()
         self.base_url = f"{BASE_URL}/student"
+        self.timeout = timeout
 
     @allure.step("Создать нового студента")
     def create_student(self, payload: dict) -> requests.Response:
-        response = self.session.post(self.base_url, json=payload, timeout=10)
+        response = self.session.post(self.base_url, json=payload, timeout=self.timeout)
         return response
 
     @allure.step("Получить список студентов")
     def get_students_list(self) -> requests.Response:
-        response = self.session.get(self.base_url, timeout=10)
+        response = self.session.get(self.base_url, timeout=self.timeout)
+        return response
+
+    @allure.step("Получить студента по ID")
+    def get_student(self, student_id) -> requests.Response:
+        response = self.session.get(f"{self.base_url}/{student_id}", timeout=self.timeout)
+        return response
+
+    @allure.step("Обновить данные студента")
+    def update_student(self, student_id, payload: dict) -> requests.Response:
+        response = self.session.put(f"{self.base_url}/{student_id}", json=payload, timeout=self.timeout)
+        return response
+
+    @allure.step("Удалить студента")
+    def delete_student(self, student_id) -> requests.Response:
+        response = self.session.delete(f"{self.base_url}/{student_id}", timeout=self.timeout)
         return response
 
 
